6b0024467253307844e2e10f00b3a21345229028
Ryan Burke <burkery@amazon.com>
2023-02-09 15:56:45 -0700

    migrate content

---
+++ source/lib/invoices-to-insights-stack.ts
@@ -0,0 +1,170 @@
+import * as cdk from "aws-cdk-lib";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Rule } from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
+import { Queue, QueueEncryption } from "aws-cdk-lib/aws-sqs";
+import { NagSuppressions } from "cdk-nag";
+import { Construct } from "constructs";
+import { InvoiceStateMachine } from "./state-machine-construct";
+import path = require("path");
+
+export class InvoicesToInsightsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new Bucket(this, "InvoicesBucket", {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      eventBridgeEnabled: true,
+      serverAccessLogsPrefix: "accesslog/",
+    });
+
+    // This DLQ stores any failures throughout the project.
+    // There are no included actors to process DLQ events.
+    const dlq = new Queue(this, "InvoicesDLQ", {
+      retentionPeriod: Duration.days(14),
+      enforceSSL: true,
+      encryption: QueueEncryption.KMS_MANAGED,
+    });
+
+    const invoiceStateMachine = new InvoiceStateMachine(
+      this,
+      "InvoiceStateMachine",
+      {
+        dlq: dlq,
+        bucket: bucket,
+      }
+    );
+
+    bucket.grantReadWrite(invoiceStateMachine.stateMachine);
+
+    /**
+     * This EventBridge Rule construct creates child resources on our behalf
+     * with resource names starting with BucketNotificationsHandler.
+     * There is a Lambda function and IAM role created.
+     * CDK Nag and Checkov identify warnings for these resources.
+     * CDK Nag suppressions are below, but Checkov suppressions cannot
+     * be added here because the child constructs are not defined here in code.
+     * Checkov warnings are CKV_AWS_111, CKV_AWS_117, CKV_AWS_115, CKV_AWS_116.
+     */
+    new Rule(this, "ebrule", {
+      eventPattern: {
+        source: ["aws.s3"],
+        detail: {
+          bucket: {
+            name: [bucket.bucketName],
+          },
+          object: {
+            key: [{ suffix: ".pdf" }],
+          },
+        },
+        detailType: ["Object Created"],
+      },
+      targets: [
+        new targets.SfnStateMachine(invoiceStateMachine.stateMachine, {
+          retryAttempts: 1,
+          maxEventAge: Duration.minutes(5),
+          deadLetterQueue: dlq,
+        }),
+      ],
+    });
+
+    // cdk-nag suppressions
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/BucketNotificationsHandler050a0587b7544547bf325f094a3db834/Role/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM4",
+          reason:
+            "Suppressing AWS managed policy warning because we dont have access to the bucket notifications handler under the hood",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/BucketNotificationsHandler050a0587b7544547bf325f094a3db834/Role/DefaultPolicy/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Suppressing AWS managed policy warning because we dont have access to the bucket notifications handler under the hood",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressions(dlq, [
+      {
+        id: "AwsSolutions-SQS3",
+        reason: "Resource is a DLQ, does not need a DLQ.",
+      },
+    ]);
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/InvoiceStateMachine/InvoicePostProcessingFunction/ServiceRole/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM4",
+          reason: "Default AWS managed Lambda basic execution role is suitable",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/InvoiceStateMachine/InvoicePostProcessingFunction/ServiceRole/DefaultPolicy/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Suppressing wildcard resource rule because textract policies don't support anything more specific",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/InvoiceStateMachine/StartQueriesFunction/ServiceRole/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM4",
+          reason: "Default AWS managed Lambda basic execution role is suitable",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/InvoiceStateMachine/StartQueriesFunction/ServiceRole/DefaultPolicy/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Suppressing wildcard resource rule because textract policies don't support anything more specific",
+        },
+      ]
+    );
+
+    NagSuppressions.addResourceSuppressionsByPath(
+      this,
+      "/InvoicesToInsightsStack/InvoiceStateMachine/StateMachine/Role/DefaultPolicy/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Suppressing policy resource wildcard warning because this state machine needs full access to the S3 bucket",
+        },
+      ]
+    );
+  }
+}
---
+++ source/lib/state-machine-construct.ts
@@ -0,0 +1,315 @@
+import * as cdk from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
+import { Effect, PolicyStatement, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { Key } from "aws-cdk-lib/aws-kms";
+import { Architecture, Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import {
+  Choice,
+  Condition,
+  Fail,
+  LogLevel,
+  StateMachine,
+  Succeed,
+  Wait,
+  WaitTime,
+} from "aws-cdk-lib/aws-stepfunctions";
+import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
+import { Construct } from "constructs";
+import path = require("path");
+
+export interface StateMachineProps {
+  dlq: Queue;
+  bucket: Bucket;
+}
+
+// Normal use of Textract takes a few seconds (up to 30s) for
+// asynchronous jobs. You can edit this value as you see fit,
+// though anything over 30s should be superfluous.
+const TEXTRACT_WAIT_DELAY_IN_S: cdk.Duration = Duration.seconds(15);
+
+/**
+ * Creates a state machine in AWS Step Functions for processing
+ * utility bill invoices into a standardized format.
+ * See the project README for a depiction of the state machine
+ * once deployed.
+ * The state machine includes two AWS Lambda functions. The
+ * startQueriesFunction checks if additional queries are present
+ * to fetch deeper insights from the utility bill. The
+ * invoicePostProcessingFunction applies the results of all
+ * Amazon Textract results and outputs the standardized data.
+ */
+export class InvoiceStateMachine extends Construct {
+  public readonly stateMachine: StateMachine;
+
+  constructor(scope: Construct, id: string, props: StateMachineProps) {
+    super(scope, id);
+
+    const invoicePostProcessingFunction = new NodejsFunction(
+      this,
+      "InvoicePostProcessingFunction",
+      {
+        architecture: Architecture.ARM_64,
+        entry: path.join(
+          __dirname,
+          "/../packages/invoicePostProcessing/index.ts"
+        ),
+        memorySize: 1024,
+        runtime: Runtime.NODEJS_18_X,
+        environmentEncryption: new Key(
+          this,
+          "InvoicePostProcessingFunctionKey",
+          {
+            enableKeyRotation: true,
+          }
+        ),
+        reservedConcurrentExecutions: 100,
+        deadLetterQueue: props.dlq,
+        tracing: Tracing.ACTIVE,
+      }
+    );
+
+    invoicePostProcessingFunction.addToRolePolicy(
+      new PolicyStatement({
+        actions: [
+          "textract:GetDocumentAnalysis",
+          "textract:GetExpenseAnalysis",
+        ],
+        resources: ["*"],
+        effect: Effect.ALLOW,
+      })
+    );
+
+    const startQueriesFunction = new NodejsFunction(
+      this,
+      "StartQueriesFunction",
+      {
+        architecture: Architecture.ARM_64,
+        entry: path.join(__dirname, "/../packages/startQueries/index.ts"),
+        memorySize: 1024,
+        runtime: Runtime.NODEJS_18_X,
+        environmentEncryption: new Key(this, "StartQueriesFunctionKey", {
+          enableKeyRotation: true,
+        }),
+        reservedConcurrentExecutions: 100,
+        deadLetterQueue: props.dlq,
+        tracing: Tracing.ACTIVE,
+      }
+    );
+
+    startQueriesFunction.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["textract:StartDocumentAnalysis"],
+        resources: ["*"],
+        effect: Effect.ALLOW,
+      })
+    );
+
+    props.bucket.grantRead(startQueriesFunction);
+
+    const waitForTextractDocumentAnalysisStep = new Wait(
+      this,
+      "WaitForTextractDocumentAnalysis",
+      {
+        time: WaitTime.duration(TEXTRACT_WAIT_DELAY_IN_S),
+        comment:
+          "Allows time for Textract to process the invoice asynchronously before attempting to check if ready",
+      }
+    );
+
+    const waitForTextractExpenseAnalysisStep = new Wait(
+      this,
+      "WaitForTextractExpenseAnalysis",
+      {
+        time: WaitTime.duration(TEXTRACT_WAIT_DELAY_IN_S),
+        comment:
+          "Allows time for Textract to process the invoice asynchronously before attempting to check if ready",
+      }
+    );
+
+    const postProcessingSteps = new tasks.LambdaInvoke(
+      this,
+      "postProcessingLambdaStep",
+      {
+        lambdaFunction: invoicePostProcessingFunction,
+      }
+    )
+      .next(
+        new tasks.CallAwsService(this, "PutObjectStep", {
+          iamResources: [props.bucket.arnForObjects("*")],
+          service: "s3",
+          action: "putObject",
+          parameters: {
+            "Body.$": "$.Payload.invoiceOutput",
+            Bucket: props.bucket.bucketName,
+            "Key.$": "$.Payload.outputKey",
+          },
+        })
+      )
+      .next(new Succeed(this, "EndSuccessStep"));
+
+    const documentAnalysisChoice = new Choice(
+      this,
+      "EvaluateGetDocumentAnalysisReadiness"
+    )
+      .when(
+        Condition.or(
+          Condition.stringEquals(
+            "$.documentAnalysis.status.JobStatus",
+            "SUCCEEDED"
+          ),
+          Condition.stringEquals(
+            "$.documentAnalysis.status.JobStatus",
+            "PARTIAL_SUCCESS"
+          )
+        ),
+        postProcessingSteps
+      )
+      .when(
+        Condition.stringEquals(
+          "$.documentAnalysis.status.JobStatus",
+          "IN_PROGRESS"
+        ),
+        waitForTextractDocumentAnalysisStep
+      )
+      .otherwise(new Fail(this, "FailedTextractQuery"));
+
+    const getDocumentAnalysisBranch = new tasks.CallAwsService(
+      this,
+      "GetDocumentAnalysisStep",
+      {
+        iamResources: ["*"],
+        service: "textract",
+        action: "getDocumentAnalysis",
+        parameters: {
+          "JobId.$": "$.queries.Payload.documentAnalysisJobId",
+          MaxResults: 1,
+        },
+        resultPath: "$.documentAnalysis.status",
+        resultSelector: {
+          "JobStatus.$": "$.JobStatus",
+        },
+      }
+    ).next(documentAnalysisChoice);
+
+    waitForTextractDocumentAnalysisStep.next(getDocumentAnalysisBranch);
+
+    const startQueriesBranch = new tasks.LambdaInvoke(
+      this,
+      "startQueriesLambdaStep",
+      {
+        lambdaFunction: startQueriesFunction,
+        resultPath: "$.queries",
+        resultSelector: {
+          "Payload.$": "$.Payload",
+        },
+      }
+    ).next(
+      new Choice(this, "StartQueriesChoiceStep", {
+        comment:
+          "Check if we need to wait for another Textract job to finish or proceed to post processing",
+      })
+        .when(
+          Condition.booleanEquals("$.queries.Payload.waitForQuery", true),
+          waitForTextractDocumentAnalysisStep
+        )
+        .otherwise(postProcessingSteps)
+    );
+
+    const getExpenseAnalysisStep = new tasks.CallAwsService(
+      this,
+      "GetExpenseAnalysis",
+      {
+        iamResources: ["*"],
+        service: "textract",
+        action: "getExpenseAnalysis",
+        inputPath: "$.expenseAnalysis.id",
+        parameters: {
+          "JobId.$": "$.JobId",
+          MaxResults: 1,
+        },
+        resultPath: "$.expenseAnalysis.status",
+        resultSelector: {
+          "JobStatus.$": "$.JobStatus",
+        },
+      }
+    );
+
+    waitForTextractExpenseAnalysisStep.next(getExpenseAnalysisStep);
+
+    const evaluateGetExpenseAnalysisReadiness = new Choice(
+      this,
+      "EvaluateGetExpenseAnalysisReadiness"
+    )
+      .when(
+        Condition.or(
+          Condition.stringEquals(
+            "$.expenseAnalysis.status.JobStatus",
+            "SUCCEEDED"
+          ),
+          Condition.stringEquals(
+            "$.expenseAnalysis.status.JobStatus",
+            "PARTIAL_SUCCESS"
+          )
+        ),
+        startQueriesBranch
+      )
+      .when(
+        Condition.stringEquals(
+          "$.expenseAnalysis.status.JobStatus",
+          "IN_PROGRESS"
+        ),
+        waitForTextractExpenseAnalysisStep
+      )
+      .otherwise(new Fail(this, "FailedTextract"));
+
+    getExpenseAnalysisStep.next(evaluateGetExpenseAnalysisReadiness);
+
+    const preProcessingSteps = new tasks.CallAwsService(
+      this,
+      "StartExpenseAnalysis",
+      {
+        iamResources: ["*"],
+        service: "textract",
+        action: "startExpenseAnalysis",
+        parameters: {
+          DocumentLocation: {
+            S3Object: {
+              "Bucket.$": "$.detail.bucket.name",
+              "Name.$": "$.detail.object.key",
+            },
+          },
+        },
+        resultPath: "$.expenseAnalysis.id",
+      }
+    ).next(waitForTextractExpenseAnalysisStep);
+
+    // new CloudWatch Logs group for the state machine
+    const stateMachineLogsKey = new Key(this, "LogsEncryptionKey", {
+      enableKeyRotation: true,
+    });
+    const stateMachineLogs = new LogGroup(this, "SfnLogs", {
+      encryptionKey: stateMachineLogsKey,
+    });
+
+    this.stateMachine = new StateMachine(this, "StateMachine", {
+      logs: {
+        destination: stateMachineLogs,
+        level: LogLevel.ALL,
+        includeExecutionData: true,
+      },
+      tracingEnabled: true,
+      definition: preProcessingSteps,
+    });
+
+    stateMachineLogsKey.grantEncryptDecrypt(
+      new ServicePrincipal(
+        ServicePrincipal.servicePrincipalName("logs.amazonaws.com")
+      )
+    );
+    stateMachineLogsKey.grantEncrypt(this.stateMachine);
+  }
+}
